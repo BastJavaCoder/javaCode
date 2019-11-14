3f695e3ec7c3e25bce174ef6283b760ec20ec0ae
BastJavaCoder <57743175+BastJavaCoder@users.noreply.github.com>
2019-11-14 14:15:10 +0800

    建造第一个javaBean
    javaBean的实现

---
+++ bean.java
@@ -0,0 +1,13 @@
+class Pojo{
+  private String pojo;
+  public void setPojo(String pojo){
+    this.pojo=pojo;    
+  }
+  public String getPojo(){
+    return this.pojo;
+  }
+  public Pojo(){}
+  public Pojo(String pojo){
+    this.pojo=pojo;
+  }
+}
